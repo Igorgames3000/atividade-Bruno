0caa8b283ac037fb090bd8f50438786334cb31a3
igorg <igorg@ArchLinux>
2026-06-04 14:56:27 -0300

    Criei os packages para cada padrao que vai usar

---
+++ AtividadeBruno/test/JUnityTeste.java
@@ -0,0 +1,18 @@
+
+import org.junit.jupiter.api.Test;
+
+
+
+/**
+ *
+ * @author igorg
+ */
+public class JUnityTeste {
+    
+    public JUnityTeste() {
+    }
+    
+    
+    @Test
+    public void hello() {System.out.println("abc");}
+}
